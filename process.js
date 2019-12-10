d7d2b5e3b7afe2f071a70c51da6b1b9a15e0ed99
zanaviska <31745527+zanaviska@users.noreply.github.com>
2019-12-10 22:01:36 +0200

    Update process.js

--- process.js
+++ process.js
@@ -1,45 +1,38 @@
+'use strict';
+
 const fs = require('fs');
 
-function parseCLArguments() {
+if (process.argv.length < 4) {
+  console.error("Not enough parameters specified, required 2, got %d", process.argv.length - 2)
+  process.exit();
+} else if (!fs.statSync(process.argv[2]).isDirectory()) {
+  console.error(process.argv[2] + " is supposed to be a directory")
+  process.exit();
+} else if (process.argv.length > 4) {
+  console.log("Ignoring all parameters except " + process.argv[2] + " and " + process.argv[3])
+}
 
-    if (process.argv.length < 4) {
-        console.error("Not enough parameters specified, required 2, got %d", process.argv.length - 2)
-        process.exit();
-    } else if (!fs.statSync(process.argv[2]).isDirectory()) {
-        console.error(process.argv[2] + " is supposed to be a directory")
-        process.exit();
-    } else if (process.argv.length > 4) {
-        console.log("Ignoring all parameters except " + process.argv[2] + " and " + process.argv[3])
-    }
+const exec = async (file, taskLocation, resultLocation) => {
+  let buf = new Buffer.alloc(2000);
+  fs.stat(taskLocation+file, (err, data) => {
+    fs.open(taskLocation+file, 'r', (err, fb) => {
+      fs.read(fb, buf, 0, 2000, Math.max(0, data.size-2000), (err, bytesRead, buffer) => {
+        let text = '';
+        for(let i = 0; i < bytesRead; i++)
+          text += String.fromCharCode(buffer[i]);
+        fs.mkdirSync(resultLocation, {recursive: true});
+        const arr = (text+'   ').split(/(?<=[.?!] )/g);
+        fs.appendFile(resultLocation + file.split('.')[0] + '.res', arr[arr.length-2], (err) => {
+          if(err) throw err;
+        })
+      })
+    })
+  });
+}
 
-    var param = {}
-
-    param.inputDir = process.argv[2]
-    param.outputDir = process.argv[3]
-
-    return param;
-};
-
-function getInputFiles(dir, filelist) {
-
-    var path = path || require('path');
-    var files = fs.readdirSync(dir),
-        filelist = (filelist === null) ? [] : filelist;
-
-    files.forEach(function(file) {
-        if (fs.statSync(path.join(dir, file)).isDirectory()) {
-            filelist = getInputFiles(path.join(dir, file), filelist);
-        } else {
-            filelist.push(path.join(dir, file));
-        }
-
-    });
-
-    return filelist;
-};
-
-const parameters = parseCLArguments();
-const inputfiles = getInputFiles(parameters.inputDir, []);
-
-
-console.log("Total number of processed files:", inputfiles.length)
+fs.readdir(process.argv[2], (err, files) => {
+  if(err) throw err;
+  files.forEach(element => {
+    setTimeout(() => (exec('/' + element, process.argv[2], process.argv[3])), 0);
+  });
+});
